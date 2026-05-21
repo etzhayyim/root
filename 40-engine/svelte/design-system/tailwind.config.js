1f005111cc4ac5b64baaf9fb80aa8167e5655ecd
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-05-21 23:04:33 +0900

    chore(migration): update etzhayyim relocation state tail batch 19

---
+++ 40-engine/svelte/design-system/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@digital-go-jp/tailwind-theme-plugin'),
+    require('@tailwindcss/container-queries'),
+  ],
+};
