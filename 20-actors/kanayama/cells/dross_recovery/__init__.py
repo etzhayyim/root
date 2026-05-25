9b7e83ff66874de3b85481a879514e566d24fbe7
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-05-25 13:59:53 +0900

    Add kanayama recycling actor

---
+++ 20-actors/kanayama/cells/dross_recovery/__init__.py
@@ -0,0 +1,5 @@
+"""Dross recovery cell (L3 cross-cutting)."""
+
+from .cell import DrossRecoveryCell
+
+__all__ = ["DrossRecoveryCell"]
---
+++ 20-actors/kanayama/cells/melting_furnace/__init__.py
@@ -0,0 +1,5 @@
+"""Melting furnace cell (L3)."""
+
+from .cell import MeltingFurnaceCell
+
+__all__ = ["MeltingFurnaceCell"]
